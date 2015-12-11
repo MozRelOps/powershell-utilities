e8c7f66f51a0debb06839cb3591e02a442dd7482
Rob Thijssen <rthijssen@gmail.com>
2015-12-11 16:05:43 -0500

    simple userdata with dsc

---
+++ userdata-example.ps1
@@ -0,0 +1,24 @@
+function Run-DesiredStateConfig {
+  param (
+    [string] $url
+  )
+  $config = [IO.Path]::GetFileNameWithoutExtension($url)
+  $target = ('{0}\{1}.ps1' -f $env:Temp, $config)
+  (New-Object Net.WebClient).DownloadFile($url, $target)
+  Unblock-File -Path $target
+  . $target
+  $mof = ('{0}\{1}' -f $env:Temp, $config)
+  & $config @('-OutputPath', $mof)
+  Start-DscConfiguration -Path $mof -Wait -Verbose -Force
+}
+
+$moztype = 'y-2012'
+
+$manifestUri = ('https://raw.githubusercontent.com/MozRelOps/powershell-utilities/master/Manifest/{0}' -f $moztype)
+$configs = @(
+  'ResourceConfig',
+  'SoftwareConfig'
+)
+foreach ($config in $configs) {
+  Run-DesiredStateConfig -url ('{0}/{1}.ps1' -f $manifestUri, $config)
+}
