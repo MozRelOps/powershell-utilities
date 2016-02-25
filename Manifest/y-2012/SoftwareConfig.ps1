ba8330052ff0457ddcc7028fc6bb7facc740f41a
Rob Thijssen <rthijssen@gmail.com>
2016-02-25 10:58:36 +0000

    taskcluster build choco pre-reqs

--- Manifest/y-2012/SoftwareConfig.ps1
+++ Manifest/y-2012/SoftwareConfig.ps1
@@ -3,11 +3,31 @@ Configuration SoftwareConfig {
   Chocolatey SublimeText3Install {
     Ensure = 'Present'
     Package = 'sublimetext3'
-    Version = '3.0.0.3083'
+    Version = '3.0.0.3103'
   }
   Chocolatey SublimeText3PackageControlInstall {
     Ensure = 'Present'
     Package = 'sublimetext3.packagecontrol'
     Version = '2.0.0.20140915'
+  }
+  Chocolatey VisualStudioCommunity2013Install {
+    Ensure = 'Present'
+    Package = 'visualstudiocommunity2013'
+    Version = '12.0.21005.1'
+  }
+  Chocolatey WindowsSdkInstall {
+    Ensure = 'Present'
+    Package = 'windows-sdk-8.1'
+    Version = '8.100.26654.0'
+  }
+  Chocolatey MozillaBuildInstall {
+    Ensure = 'Present'
+    Package = 'MozillaBuild'
+    Version = '2.0.0'
+  }
+  Chocolatey RustInstall {
+    Ensure = 'Present'
+    Package = 'rust'
+    Version = '1.6.0'
   }
 }
